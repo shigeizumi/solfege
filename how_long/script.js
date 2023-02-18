7f0d25db868a8f9cc7dd9e75cdd764583ed61829
shigeizumi <106294022+shigeizumi@users.noreply.github.com>
2023-02-18 14:56:46 +0900

    Update script.js

--- how_long/script.js
+++ how_long/script.js
@@ -3,31 +3,34 @@ function make_problem(){
     let position=0;
     let long=[1,2,2,4,4,8];
     let problem=[];
-    while(position<16){
-        let num=Math.floor(Math.random()*long.length);
-        if(position+long[num]>16){
-            let number=parseInt(Number((16-position).toString(2).replace(/1/g,"0").replace(/^0/,"1")),2) ;
-            problem.push(number);
-            position+=number;
+    for(let t=0;t<2;t++){
+        while(position<8+8*t){
+            let num=Math.floor(Math.random()*long.length);
+            if(position+long[num]>8+8*t){
+                let number=parseInt(Number((8+8*t-position).toString(2).replace(/1/g,"0").replace(/^0/,"1")),2) ;
+                problem.push(number);
+                position+=number;
+            }
+            else {
+                problem.push(long[num]);
+                position+=long[num];
+            }
         }
-        else {
-            problem.push(long[num]);
-            position+=long[num];
+        //8分音符が連続していたらティティにする
+        //確率で４分音符を４分休符にする
+        for(let i=0;i<problem.length;i++){
+            if(problem[i]==2&&problem[i+1]==2){
+                problem=problem.filter((element,index)=>index!=i);
+                problem[i]=22;
+                i=0;
+            }
+            if(problem[i]==4){
+                let dice=Math.floor(Math.random()*6);
+                if(dice==0) problem[i]="4_1";
+            }
         }
     }
-    //8分音符が連続していたらティティにする
-    //確率で４分音符を４分休符にする
-    for(let i=0;i<problem.length;i++){
-        if(problem[i]==2&&problem[i+1]==2){
-            problem=problem.filter((element,index)=>index!=i);
-            problem[i]=22;
-            i=0;
-        }
-        if(problem[i]==4){
-            let dice=Math.floor(Math.random()*6);
-            if(dice==0) problem[i]="4_1";
-        }
-    }
+    
     //１箇所を空欄にする
     let boxnum=Math.floor(Math.random()*problem.length);
     let answer=problem[boxnum];
